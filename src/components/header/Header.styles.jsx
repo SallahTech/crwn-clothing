4a36f8fdfa1e0265ef9e6ac1efc67abaec7e713a
SallahTech <DonlyRlMrKtr@gmail.com>
2020-11-14 22:18:58 +0200

    Change the and customButtom component styles to use styled-components

--- src/components/header/Header.styles.jsx
+++ src/components/header/Header.styles.jsx
@@ -1,10 +1,5 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-
-const OptionContainerStyles = css`
-  cursor: pointer;
-  padding: 10px 15px;
-`
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -29,9 +24,6 @@ export const OptionsContainer = styled.div`
 `
 
 export const OptionLink = styled(Link)`
-  ${OptionContainerStyles}
-`
-
-export const OptionDiv = styled.div`
-  ${OptionContainerStyles}
+  cursor: pointer;
+  padding: 10px 15px;
 `
